fc1a76d41a03a20e14138e96b0782d01406c29bc
Soll <artemzlat@mail.ru>
2015-08-31 12:03:45 +0500

    PGL logic added

--- app/models/price_group_line.rb
+++ app/models/price_group_line.rb
@@ -2,6 +2,7 @@ class PriceGroupLine < ActiveRecord::Base
   belongs_to :item
   belongs_to :price_group
 
+  validates :price_group_id, uniqueness: { scope: [:item_id, :amount] }
   validates :amount, :price, presence: true
   validates :amount, numericality: { only_integer: true }
   validates_format_of :price, with: /\A\d+(.\d{0,2})?\z/i,
