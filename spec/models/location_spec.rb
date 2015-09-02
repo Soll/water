6fe1a9f69dccb00c64e45dc8ed911306ac600239
Soll <artemzlat@mail.ru>
2015-09-02 13:14:17 +0500

    Typo correct

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -2,6 +2,6 @@ require "rails_helper"
 
 describe Location do
   it "should create a valid factory" do
-  	expect(FactoryGirl.create(:location)).to be_valid
+    expect(FactoryGirl.create(:location)).to be_valid
   end
 end
